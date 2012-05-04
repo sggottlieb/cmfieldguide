d4b676a60fc4b90ca1609b11aa653094c1237d19
Seth Gottlieb <seth@contenthere.net>
2012-05-03 20:35:12 -0400

    added kentico

---
+++ cmfieldguide/cmsdetector/signatures/kentiko.py
@@ -0,0 +1,32 @@
+"""
+This signature containts test to see if the site is running on Kentico.
+"""
+__author__ = "Seth Gottlieb"
+__copyright__ = "CM Fieldguide"
+__credits__ = ["Seth Gottlieb",]
+__license__ = "Unlicense"
+__version__ = "0.1"
+__maintainer__ = "Seth Gottlieb"
+__email__ = "sgottlieb@alumni.duke.edu"
+__status__ = "Experimental"
+
+
+from cmfieldguide.cmsdetector.signatures import BaseSignature
+
+class Signature(BaseSignature):
+
+    NAME = 'Kentiko'
+    WEBSITE = 'http://www.kentico.com'
+    KNOWN_POSITIVE = 'http://www.kentico.com'
+    TECHNOLOGY = '.NET'
+
+    def test_has_getcss(self, site):
+        """
+        Kentico has a GetCSS page that retrieve stylesheets by name.
+        """
+        if site.home_page.has_matching_tag( 'link', { 'rel': 'stylesheet', 'href': 'GetCSS\.aspx\?stylesheetname' } ):
+            return 1
+        else:
+            return 0
+            
+    
