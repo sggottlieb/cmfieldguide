5c65876ebd7fb642831c008b6a8ac0b07634bdbc
Seth Gottlieb <seth@contenthere.net>
2012-08-10 16:36:38 -0400

    improved opentext signature

--- cmfieldguide/cmsdetector/signatures/opentext.py
+++ cmfieldguide/cmsdetector/signatures/opentext.py
@@ -30,6 +30,7 @@ class Signature(BaseSignature):
             """
             *Some* sites running version 7.x to 9.x of OpenText Web Site Management (formerly RedDot / RedDot Solutions) will contain this
             """
+            return 1
         elif site.home_page.contains_pattern("<!--\sPageID\s\d{1,6}\s-\spublished by Open Text Web Solutions"):
             """
             *Some* sites running version 9.x to 10.x of OpenText Web Site Management will contain this
@@ -39,6 +40,28 @@ class Signature(BaseSignature):
             """
             Sites running version 11.x of OpenText Web Site Management will contain this
             """
+            return 1
+        else:
+            return 0
+    
+
+    def test_has_rde_tag(self, site):
+        """
+        RedDot components sometime embed special rde tags.
+        """
+
+        if site.home_page.contains_any_pattern(['<rde-dm:attribute', '<rde-dm:dynaments']):
+            return 1
+        else:
+            return 0
+
+    
+    def test_has_native_link_format(self, site):
+        """
+        By default, RedDot creates dynamic links of the pattern host/cps/rde/xchg/{project}/{xsl}/{content}
+        """
+
+        if site.home_page.has_matching_tag('a', {'href':'/cps/rde/xchg'}):
             return 1
         else:
             return 0
